e81308354d2a6553245a89618fbe7532ac4838d2
DumithLusena <dumithlusena123@gmail.com>
2025-11-19 16:53:55 +0530

    feat: Remove Razorpay payment method; update order controller and routes, and clean up assets

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -115,11 +115,6 @@ const verifyStripe = async (req, res) => {
 
 }
 
-// Placing orders using Razorpay Method
-const placeOrderRazorpay = async (req, res) => {
-
-}
-
 // All Orders data for Admin Panel
 const allOrders = async (req, res) => {
     try {
@@ -160,4 +155,4 @@ const updateStatus = async (req, res) => {
     }
 }
 
-export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe }
+export { placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus, verifyStripe }
